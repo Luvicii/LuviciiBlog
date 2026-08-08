0df8d63437aa6c13c2ea6ec16d15e98073932892
lrxin <unity_l@qq.com>
2026-08-08 23:19:30 +0800

    build: add gulp minification for HTML/CSS/JS in public/

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, parallel } = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
+const terser = require('gulp-terser');
+
+const pub = 'public';
+
+function minifyHtml() {
+  return src(`${pub}/**/*.html`)
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true,
+      minifyJS: false,
+      minifyCSS: false
+    }))
+    .pipe(dest(pub));
+}
+
+function minifyCss() {
+  return src(`${pub}/**/*.css`)
+    .pipe(cleanCSS())
+    .pipe(dest(pub));
+}
+
+function minifyJs() {
+  return src(`${pub}/**/*.js`)
+    .pipe(terser())
+    .pipe(dest(pub));
+}
+
+exports.min = parallel(minifyHtml, minifyCss, minifyJs);
